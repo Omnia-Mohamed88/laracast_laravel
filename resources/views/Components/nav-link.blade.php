7e8aa10bdaa33f490d77cbce7174a061dc5cf430
Omnia Salama <Omnia_salama@ETAX.COM.EG>
2024-07-29 14:41:53 +0300

    Style the Currently Active Navigation Link

--- resources/views/Components/nav-link.blade.php
+++ resources/views/Components/nav-link.blade.php
@@ -1,1 +1,4 @@
-<a {{ $attributes }}>{{ $slot }}</a>
+
+<!-- <a {{ $attributes }}>{{ $slot }}</a> -->
+ @props(['active' => false])
+<a class="{{ $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }}" aria-current="{{ $active ? 'page' : 'false' }}" {{ $attributes }}>{{ $slot }}</a>
